d3484372b7f717a40d4f444856aade3469287ae2
Xrofe <70887592+keesung98@users.noreply.github.com>
2023-03-06 17:44:44 +0900

    Update readTextFile.kt

--- readTextFile.kt
+++ readTextFile.kt
@@ -1,6 +1,6 @@
 private fun readTextFile(fullPath: String) :String {
         val file = File(fullPath)
-        if(!file.exists()) return "" //파일이 존재하지 않을때
+        if(!file.exists()) return "" //if no exists file
 
         val reader = FileReader(file)
         val buffer = BufferedReader(reader)
@@ -9,7 +9,7 @@ private fun readTextFile(fullPath: String) :String {
         var result = StringBuffer()
 
         while(true){
-            temp = buffer.readLine() // 줄단위로 read
+            temp = buffer.readLine() // line read
             if(temp == null) break
             else result.append(temp).append("\n")
         }
